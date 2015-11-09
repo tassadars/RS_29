8243ebc726a091ad935a5c54c7b75c5948ddd085
eugen <tassadars@gmail.com>
2015-11-09 09:10:26 +0200

    Add view visit and app process code

--- app.rb
+++ app.rb
@@ -19,3 +19,28 @@ get '/' do
 	@barbers = Barber.order "id DESC"
 	erb :index
 end
+
+
+before '/visit' do
+	@barbers = Barber.order "id DESC"
+end
+
+get '/visit' do
+  erb :visit
+end
+
+post '/visit' do
+  session[:username] = params['username']
+  session[:phone] = params['phone']
+  session[:datetime] = params['datetime']
+  session[:barber] = params['barber']
+  session[:color] = params['color']
+
+
+  session[:username] = ''
+  session[:phone] = ''
+  session[:datetime] = ''
+  session[:barber] = ''
+  session[:color] = ''
+  erb 'Дорогой <%=session[:username]%>, ваша заявка принята на рассмотрение. Парикмахер <%=session[:barber]%> вам перезвонит!'
+end
